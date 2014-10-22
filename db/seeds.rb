4c584b411de15add2adc2214687fbe153166ba38
lwschjang <leamia.schjang@gmail.com>
2014-10-22 19:09:19 -0400

    added my artwork, but need to do more styling due to links not being visible on white backgrounds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user1 = User.create(name: 'Will', email: 'lwschjang@gmail.com', password: 'password', password_confirmation: 'password')
 
-art1 = Art.create(title: 'King', pic_url: '1.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
-art2 = Art.create(title: 'Stolen Hearts', pic_url: '2.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
-art3 = Art.create(title: 'Suits & Ties', pic_url: '3.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
-art4 = Art.create(title: 'Kunoichi', pic_url: '4.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
+art1 = Art.create(title: 'King', pic_url: 'king.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
+art2 = Art.create(title: 'Stolen Hearts', pic_url: 'steal-your-heart3.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
+art3 = Art.create(title: 'Suits & Ties', pic_url: 'tmnt.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
+art4 = Art.create(title: 'Kunoichi', pic_url: 'Kunoichi.jpg', purchase_at: 'http://www.designbyhumans.com/shop/FutureEmperor/', user_id: user1.id)
 
 
